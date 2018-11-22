987cfadb82eccd1126de2052b102fe6d7caa2654
SinniK <Sinnik@fastfixs-mbp.home>
2018-11-22 18:27:54 +0100

    Modify the setup.py file

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/env python
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(name='gas_uk',
       version='0.0.1',
       description='This package includes the modelling approach for gas consumption in the UK.',
       author='Nikos Nezeritis',
       author_email='nikolaosnezeritis@gmail.com',
-      packages=find_packages()
+      scripts=['model'] 
     )
