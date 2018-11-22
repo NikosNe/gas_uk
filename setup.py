a05e8a9f8038f0976295b3cbcec329789980fba9
SinniK <Sinnik@fastfixs-mbp.home>
2018-11-22 19:02:13 +0100

    Attempt to improve the packaging

--- setup.py
+++ setup.py
@@ -2,12 +2,24 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/env python
 
-from setuptools import setup
+import setuptools
 
-setup(name='gas_uk',
-      version='0.0.2',
-      description='This package includes the modelling approach for gas consumption in the UK.',
-      author='Nikos Nezeritis',
-      author_email='nikolaosnezeritis@gmail.com',
-      scripts=['model'] 
-    )
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gas_uk",
+    version="0.0.2",
+    author="Nikolaos Nezeritis",
+    author_email="nikolaosnezeritis@gmail.com",
+    description="A package with the modelling process for the gas consumption timeseries somewhere in the UK",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/NikosNe/gas_uk",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
